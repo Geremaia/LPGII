070faa87650eab4fd9e9aeae251e74bbd482d56b
Geremia <leonardogeremiasc@gmail.com>
2017-08-22 09:22:43 -0300

    atualizando as coisas

---
+++ site4/index.php
@@ -0,0 +1,32 @@
+ <!DOCTYPE html>
+
+<html lang="pt-BR">
+    <head>
+        <title> Iniciando PHP</title>
+        <link rel="stylesheet" href="css/bootstrap-reboot.css"/>
+        <link rel="stylesheet" href="css/bootstrap.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
+
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"
+    </head>
+    <body>
+        <div class = "container">
+            <form action="somar.php" method="GET"/>
+                <label for="num1">Número 1</label>
+                <input type="num1" id="num1" type="number"/>
+                <br/>
+                <label for="num2">Número 2</label>
+                <input type="num2" id="num2" type="number"/>
+                <br/>
+                <input type="submit" value="Calcular"/>
+            </form>
+            <?php
+
+            ?>
+        </div>
+        <script src = "https://code.jquery.com/jquery-3.2.1.slim.min.js" int></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.1"></script>
+        <script src="js/bootstrap.js"></script>
+    </body>
+</html>
